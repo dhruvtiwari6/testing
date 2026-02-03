c845b5e2b53c1b4080ec965dae47ca3a860fca24
dhruvtiwari6 <dhurvtiwari6@gmail.com>
2026-02-03 17:20:55 +0530

    testing purpose

--- example1/app.js
+++ example1/app.js
@@ -15,7 +15,7 @@ const io = new Server(httpServer, {
 
 const mediaCodecs = [
   { kind: 'audio', mimeType: 'audio/opus', clockRate: 48000, channels: 2 },
-  { kind: 'video', mimeType: 'video/VP8', clockRate: 90000 }
+  { kind: 'video', mimeType: 'video/VP8', clockRate: 90000}
 ];
 
 let worker;
